cc2dab77bca8c2638c880ab5539fb929b64a6e57
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-05-11 16:41:46 +0530

    created a http handler

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("serverrr")
+	http.HandleFunc("/greet", greet)
+	http.ListenAndServe(":8081", nil)
+}
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type","Application/json")
+	w.Write([]byte("hello from golang"))
 }
