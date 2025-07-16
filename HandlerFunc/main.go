51ea9f48521aa5397280635f77c94371f20d6b92
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2025-07-16 12:28:54 +0530

    learned route handling

---
+++ HandlerFunc/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func call(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello from call func"))
+}
+
+func main() {
+	fmt.Println("HandlerFunc")
+
+	//adapter pattern
+	// res := compute(8, 2, func(a, b int) int { return a - b })
+	// res := compute(8, 2, add)
+	// fmt.Println(res)
+
+	//creating a new ServeMux router
+	// mux := http.NewServeMux()
+
+	http.HandleFunc("/call", call)
+	// http.ListenAndServe(":8080", &custom{})
+	http.ListenAndServe(":8080", nil)
+
+}
+
+//since interfaces can only implemented by having struct,so here there is dummy custom struct..
+// type custom struct {
+// }
+
+//custom ServerHTTP handler
+// func (*custom) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("hello from custom handler"))
+// }
+
+// adapter pattern
+type operation func(a int, b int) int
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func compute(a int, b int, op operation) int {
+	return op(a, b)
+}
+
+// ServeHTTP dispatches the request to the handler whose
+// pattern most closely matches the request URL.
+// func (mux *ServeMux) ServeHTTP(w ResponseWriter, r *Request) {
+// 	if r.RequestURI == "*" {
+// 		if r.ProtoAtLeast(1, 1) {
+// 			w.Header().Set("Connection", "close")
+// 		}
+// 		w.WriteHeader(StatusBadRequest)
+// 		return
+// 	}
+// 	var h Handler
+// 	if use121 {
+// 		h, _ = mux.mux121.findHandler(r)
+// 	} else {
+// 		h, r.Pattern, r.pat, r.matches = mux.findHandler(r)
+// 	}
+// 	h.ServeHTTP(w, r)
+// }
+
+// The above code is where the actual function is found and called.
+// Like if we define http.HandleFunc("/home",func()..) means the go's default ServeMux registers the
+// handler func with its endpoint inside a map.Then if a req hits for a specific endpoint means ServeMux's default
+// ServeHTTP method is called, where it finds the crct handler func using several built-in methods(look inside server.go file) ,
+// if found it calls the respective method
--- net/http/main.go
+++ net/http/main.go
@@ -9,9 +9,28 @@ type server struct {
 	addr string
 }
 
+// actual implementation of ServeHTTP(http.ResponseWriter, *http.Request) occurs.Since this method statisfies that handler func.
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello from server"))
+	// w.Write([]byte("hello from server"))
+	switch r.Method {
+	case http.MethodGet:
+		switch r.URL.Path {
+		case "/":
+			w.Write([]byte("hello from home endpoint"))
+			return
+		case "/test":
+			w.Write([]byte("hello from test endpoint"))
+			return
+		default:
+			w.Write([]byte("404 not found"))
+			return
+		}
+	}
 }
+
+// so http.ListenAndServe() takes two arguments ,one is port no and the other is a handler which handles the
+// incoming req. so here i have manually created a struct which has addr(port number)
+// and that struct actually implements the handler func (ServeHTTP)
 func main() {
 	fmt.Println("net/http")
 
